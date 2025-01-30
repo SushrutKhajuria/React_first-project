ddbda54f3d0f3770945f07becd34b660009ea546
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-01-30 19:43:24 +0530

    Styling component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,18 @@
-function ExpenseItem(){
-    return (
-    <div>
-        <div>
-         15 August 2023
-        </div>
+// Write your code at relevant places in the code below:
 
-        <div>
-          Book
-        </div>
+import "./ExpenseItem.css";
 
-        <div>
-         $10
-        </div>
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
+      </div>
+    </div>
+  );
+}
 
-        <div>
-         Delhi
-        </div>
-   </div>  
-  )
-  }
-  
-  
-  
-  export default ExpenseItem;
+export default ExpenseItem;
