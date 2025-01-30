0f968accc0a205ee7a0016c5d48ccba89f0640b8
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-01-30 21:19:54 +0530

    Passingobj and data to child component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
-// Write yoour code at relevant places in the code below:
+// Write your code at relevant places in the code below:
 
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 7, 15).toISOString();
-  const expenseTitle = "Insurance";
-  const expenseAmount = 50;
-  const expenseLocation ="Bangalore"
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__location">
-        {expenseLocation}
+        {props.location}
       </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
