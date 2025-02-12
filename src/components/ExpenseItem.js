414d5d8a169042143abf9954999a18701f20e5c6
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-02-12 18:11:01 +0530

    Formatting Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,27 @@
-// Write your code at the relevant places in the code below:
+import "./ExpenseItem.css"
 
-import "./ExpenseItem.css";
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 function ExpenseItem(props) {
+
+  const month = months[props.date.getMonth()];
+  const year = props.date.getFullYear();
+  const date = props.date.getDate().toString().padStart(2, "0");;
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-
-      <div className="expense-item__location">{props.location}</div>
+      <div>
+        <div> {month} </div>
+        <div> {year}</div>
+        <div> {date}</div>
+      </div>
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        {<h2>{props.title}</h2>}
         <div className="expense-item__price">${props.price}</div>
-        
       </div>
     </div>
-  );
+  )
 }
 
 export default ExpenseItem;
